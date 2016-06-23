e5721c22c3aa5f9fa8dcfebade4db2d319708496
Vytau <vytautas.skinkys@gmail.com>
2016-06-23 22:02:41 +0200

    feat: agencies can reject offer and all rejections are loged in file

--- src/Gateways/BrokerAgencyGateway.java
+++ src/Gateways/BrokerAgencyGateway.java
@@ -10,25 +10,43 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 import java.util.HashMap;
 
 /**
  * Created by Vytautas on 21-Jun-16.
  */
 public abstract class BrokerAgencyGateway {
-    MessageSenderGateway sender;
-    MessageSenderGateway sender1;
-    MessageSenderGateway sender2;
+    MessageSenderGateway bookFastSender;
+    MessageSenderGateway bookCheapSender;
+    MessageSenderGateway bookGoodSender;
     MessageReceiverGateway receiver;
     AgencySerializer serializer;
     private HashMap<String, AgencyRequest> cach = new HashMap<>();
 
+    File file = new File("rejectedList.txt");
+    FileWriter writer;
+
     public BrokerAgencyGateway(){
-        sender = new MessageSenderGateway("bookFastQueue");
-        sender1 = new MessageSenderGateway("bookCheapQueue");
-        sender2 = new MessageSenderGateway("bookGoodServiceQueue");
+        bookFastSender = new MessageSenderGateway("bookFastQueue");
+        bookCheapSender = new MessageSenderGateway("bookCheapQueue");
+        bookGoodSender = new MessageSenderGateway("bookGoodServiceQueue");
         receiver = new MessageReceiverGateway("agencyReplyChanel");
         serializer = new AgencySerializer();
+
+        try {
+            writer = new FileWriter(file, true);
+            PrintWriter printer = new PrintWriter(writer);
+            Date d = new Date();
+            printer.append(d.toString() + System.lineSeparator());
+            printer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         receiver.setListener(new MessageListener() {
             @Override
@@ -38,8 +56,17 @@ public abstract class BrokerAgencyGateway {
                     msgText = ((TextMessage) msg).getText();
                     AgencyReply agencyReply = serializer.agencyReplyFromString(msgText);
                     AgencyRequest agencyRequest = cach.get(msg.getJMSCorrelationID());
-                    onAgencyReply(agencyReply, agencyRequest);
+                    if(agencyReply.getTotalPrice() >= 0){
+                        onAgencyReply(agencyReply, agencyRequest);
+                    } else {
+                        writer = new FileWriter(file, true);
+                        PrintWriter printer = new PrintWriter(writer);
+                        printer.append("Request: " + agencyRequest +" was rejected by: " +agencyReply.getNameAgency() + System.lineSeparator());
+                        printer.close();
+                    }
                 } catch (JMSException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
@@ -50,17 +77,23 @@ public abstract class BrokerAgencyGateway {
         try {
             String serMsg = serializer.agencyRequestToString(request);
 
-            Message msg = sender.createMessage(serMsg);
-            sender.sendMessage(msg);
-            cach.put(msg.getJMSMessageID(), request);
+            if(request.getTransferDistance() == 0) {
+                Message msg = bookFastSender.createMessage(serMsg);
+                bookFastSender.sendMessage(msg);
+                cach.put(msg.getJMSMessageID(), request);
+            }
 
-            Message msg1 = sender1.createMessage(serMsg);
-            sender1.sendMessage(msg1);
-            cach.put(msg1.getJMSMessageID(), request);
+            if(request.getTransferDistance() >= 10 && request.getTransferDistance() <= 50) {
+                Message msg1 = bookCheapSender.createMessage(serMsg);
+                bookCheapSender.sendMessage(msg1);
+                cach.put(msg1.getJMSMessageID(), request);
+            }
 
-            Message msg2 = sender2.createMessage(serMsg);
-            sender2.sendMessage(msg2);
-            cach.put(msg2.getJMSMessageID(), request);
+            if(request.getTransferDistance() <= 40) {
+                Message msg2 = bookGoodSender.createMessage(serMsg);
+                bookGoodSender.sendMessage(msg2);
+                cach.put(msg2.getJMSMessageID(), request);
+            }
         } catch (JMSException e) {
             e.printStackTrace();
         }
